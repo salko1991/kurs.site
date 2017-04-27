7984dc0e678ed501cc8ea22944692c1317b1e046
Dmitriy <salko91@i.ua>
2017-04-27 11:29:13 +0300

    dz

---
+++ DZ3.php
@@ -0,0 +1,92 @@
+<?php
+//Задача 1
+echo '<h3>Задача 1</h3>';
+echo '<table>';
+for($i = 0; $i < 10; $i++){
+	echo '<tr>';
+	for($j = 0; $j < 10; $j++){
+		if(($i +$j) % 2 == 1){
+			echo '<td class="tdBlack"></td>';
+		}else{
+			echo '<td></td>';	
+		}
+	}
+	echo '</tr>';
+}
+echo '</table>';
+echo '<br />';
+//Задача 1
+
+//Задача 2
+echo '<h3>Задача 2</h3>';
+$taskTwo_arr = array();
+$taskTwoSumThree_int = 0;
+for ($i=0; $i < 10; $i++) { 
+	$taskTwo_arr[$i] = rand(1, 10);
+	if(($i +1) % 3 == 0){
+		$taskTwoSumThree_int += $taskTwo_arr[$i];
+	}
+}
+echo '<br />Итоговый массив: ';
+print_r($taskTwo_arr);
+echo '<br />';
+echo 'Сумма каждого 3 элемента в массиве: '.$taskTwoSumThree_int;
+echo '<br />';
+//Задача 2
+
+//Задача 3
+echo '<h3>Задача 3</h3>';
+$taskThree_arr = array();
+$temp_int;
+for ($i=0; $i < 10; $i++) { 
+	$temp_int = rand(1, 10);
+	if(in_array($temp_int, $taskThree_arr)){
+		echo 'число '.$temp_int.' находиться в массиве '.print_r($taskThree_arr, true).'<br />';
+	}
+	$taskThree_arr[$i] = $temp_int;
+}
+echo '<br />Итоговый массив: ';
+print_r($taskThree_arr);
+echo '<br />';
+//Задача 3
+
+//Задача 4
+echo '<h3>Задача 4</h3>';
+$taskFour_arr = array();
+$taskFive_arr = array();
+$taskSixDiff_arr = array();
+for ($i=0; $i < 20; $i++) {
+	$taskFour_arr[] = rand(1, 20);
+	$taskFive_arr[] = rand(1, 20);
+}
+$taskSixDiff_arr = array_diff($taskFour_arr, $taskFive_arr);
+echo '<br />';
+echo 'Массив1: ';
+print_r($taskFour_arr);
+echo '<br />';
+echo 'Массив2: ';
+print_r($taskFive_arr);
+echo '<br />';
+echo 'Массив из элементов массива1, которых нет в массиве2: ';
+print_r($taskSixDiff_arr);
+echo '<br />';
+//Задача 4
+
+//Задача 5
+echo '<h3>Задача 5</h3>';
+require_once 'pagesArray.php';
+//Задача 5
+?>
+<ul>
+	<?php foreach($dataPages_arr as $page) { ?>
+		<?php if((int)$page->visible == 1 && (int)$page->menu_id == 1){ ?>
+			<li><a href="<?php echo $page->url; ?>"><?php echo $page->name; ?></a></li>
+		<?php } ?>
+	<?php } ?>
+</ul>
+<?php
+//Задача 6
+require_once 'productsArray.php';
+//Задача 6
+?>
+<h3><a href="task_six.php">Задача 6</a></h3>
--- DZ4.php
+++ DZ4.php
@@ -1,6 +1,4 @@
 <br />
-============================================================================================
-<br />
 <?php
 //1
 function drawTable($col = 10, $row = 10, $color = '#c299ef', $even = true) {
@@ -43,27 +41,9 @@ showMenu($dataPages_arr, false);
 showMenu($dataPages_arr, true);
 //2
 //4
+echo '<h3>Задание 4</h3>';
 require 'categories_array.php';
 
-/*function buildCategories(&$categories_data, $categoryItem = null) {
-	if ($categories_data) {
-		foreach ($categories_data as $key => $category) {
-			if (is_null($categoryItem)) {
-				if ($category->parent_id != 0) {
-					unset($categories_data[$key]);
-					buildCategories($categories_data, $category);
-				}
-			} else {
-				if ($category->id == $categoryItem->parent_id) {
-					$category->subcategories[] = $categoryItem;
-				} elseif (isset($category->subcategories) && !empty($category->subcategories)) {
-					buildCategories($category->subcategories, $categoryItem);
-				}
-			}
-		}
-	}
-}*/
-//$categories_data1 = $categories_data;
 function buildCategories(&$categories_data, $categoryItem = null) {
 	if ($categories_data) {
 		foreach ($categories_data as $key => $category) {
@@ -78,44 +58,7 @@ function buildCategories(&$categories_data, $categoryItem = null) {
 		}
 	}
 }
-$start = microtime(true);
-//buildCategories($categories_data1);
-//$build = microtime(true) - $start;
-//echo 'Время buildCategories: '.$build.' сек.';
-$new_categories = GetCategoriesTree($categories_data);
-$getC = microtime(true) - $start;
-echo 'Время GetCategoriesTree: '.$getC.' сек.';
-
-
-function GetCategoriesTree($categories,$parent_id=0) {   
-    $results=array();
-    if ($categories) {
-        foreach ($categories as $key => $category) {
-            if ($category->parent_id==$parent_id) {
-                $result=new stdClass();
-                $result->name=$category->name;
-                $result->url=$category->url;
-                $result->visible=$category->visible;
-                if ($category->id!=$parent_id) 
-                	$result->subcategories=GetCategoriesTree($categories,$category->id);
-                $results[]=$result;
-                unset($result);
-            }
-        }        
-    }
-    return $results;
-}
-$start = microtime(true);
-//$new_categories = GetCategoriesTree($categories_data);
-//$getC = microtime(true) - $start;
-//echo 'Время GetCategoriesTree: '.$getC.' сек.';
-buildCategories($categories_data1);
-$build = microtime(true) - $start;
-echo 'Время buildCategories: '.$build.' сек.';
-echo '<br />GetCategoriesTree - buildCategories'.($getC - $build);
-//print_r($new_categories);
-//print_r();
-echo '<br />==================================<br />';
+buildCategories($categories_data);
 function getCategories($categories) {
     if($categories) { // проверка лишней не бывает
         echo "<ul>";
@@ -131,7 +74,6 @@ function getCategories($categories) {
         echo "</ul>";
     }
 }
-getCategories($categories_data1);
-getCategories($new_categories);
+getCategories($categories_data);
 //4
 ?>
